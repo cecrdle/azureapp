1bc9935a6dcfb9f6de7f81433bd7b4b1d155d1c4
cecrdlem <cecrdle.milan@hypotecnibanka.cz>
2021-02-20 22:52:59 +0100

    first test projekt

---
+++ src/test/java/cz/littlestars/azure/spring/workshop/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.littlestars.azure.spring.workshop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
